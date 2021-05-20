fc172e8f1fcf1175e959fc6fdf4121fb4d3a0048
SWAPNIL MISHRA <swapnilmishra2808@gmail.com>
2021-05-21 02:45:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function compute() {
+    var principal = document.getElementById("principal").value;
+    if(principal=="" || principal<1){
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+    }
+    else{
+        var principal = parseFloat(document.getElementById("principal").value);
+        var rate = parseFloat(document.getElementById("rate").value);
+        var years = parseInt(document.getElementById("years").value);
+        var interest = principal * years * rate / 100;
+        var yearInTheFuture = new Date().getFullYear() + years;
+        document.getElementById("result").innerHTML = "If you deposit <mark>" + principal + "</mark>,<br/>" +
+        "at an interest rate of <mark>" + rate + "%</mark>,<br/>" +
+        "You will receive an amount of <mark>" + interest + "</mark>,<br/> " +
+        "in the year <mark>" + yearInTheFuture + "</mark>";
+    }
 }
-        
+
+function slider(){
+    var slide  = document.getElementById("rate").value;
+    document.getElementById("rate_value").innerHTML= slide +"%";
+}
